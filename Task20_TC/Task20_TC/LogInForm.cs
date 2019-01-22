0d1fdd0a690bb24b9be5a9b5f807b99db8d2f688
NastyaZhilinskaya <nastyazhilinskaya@coherentsolutions.com>
2019-01-22 13:49:29 +0300

    Add task

---
+++ Task20_TC/Task20_TC/LogInForm.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Task20_TC
+{
+    class LogInForm
+    {
+        private IWebDriver driver;
+        private By EnterButton = By.LinkText("Войти");
+        private By InputEmailButton = By.Name("login");
+        private By InputPasswordButton = By.XPath("//input[@name = 'password']");
+        private By SubmitButton = By.CssSelector(".auth__enter");
+        private By UserNameButton = By.ClassName("uname");
+
+        public LogInForm(ChromeDriver chromeDriver)
+        {
+            driver = chromeDriver;
+        }
+
+        public void LogIn(string userEmail, string userPassword)
+        {
+            driver.FindElement(EnterButton).Click();
+            driver.FindElement(InputEmailButton).SendKeys(userEmail);
+            driver.FindElement(InputPasswordButton).SendKeys(userPassword);
+            driver.FindElement(SubmitButton).Click();
+        }
+
+        public string GetInformationAboutUserName()
+        {
+            string value = driver.FindElement(UserNameButton).Text;
+            return value;
+        }
+
+    }
+}
---
+++ Task20_TC/Task20_TC/LoginTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Task20_TC
+{
+    [TestFixture]
+    class LoginTests
+    {
+        private const string USEREMAIL = "seleniumtests@tut.by";
+        private const string USERPASSWORD = "123456789zxcvbn";
+        
+        private ChromeDriver chromeDriver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+            chromeDriver.Url = "https://www.tut.by/";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            chromeDriver.Quit();
+        }
+
+        [Test]
+        [TestCase("Selenium Test")]
+        public void CorrectUserLogin(string expectedResult)
+        {
+            LogInForm logIn = new LogInForm(chromeDriver);
+            logIn.LogIn(USEREMAIL, USERPASSWORD);
+            Assert.AreEqual(expectedResult, logIn.GetInformationAboutUserName());
+        }
+    }
+}
