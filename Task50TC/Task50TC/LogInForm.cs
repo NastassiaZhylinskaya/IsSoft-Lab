bf7e4304e64bd7a36a723a11c95d7958103b37a5
NastyaZhilinskaya <nastyazhilinskaya@coherentsolutions.com>
2019-02-04 11:13:10 +0300

    Add task with Tut.by.

---
+++ Task50TC/Task50TC/LogInForm.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Task50TC
+{
+    public class LogInForm : BasePage
+    {
+        private IWebDriver driver;
+        private By EnterButton = By.LinkText("Войти");
+        private By InputEmailButton = By.Name("login");
+        private By InputPasswordButton = By.XPath("//input[@name = 'password']");
+        private By SubmitButton = By.CssSelector(".auth__enter");
+        private By UserNameButton = By.ClassName("uname");
+
+        public LogInForm(ChromeDriver chromeDriver)
+        {
+            driver = chromeDriver;
+        }
+
+        public void LogIn(string userEmail, string userPassword)
+        {
+            driver.FindElement(EnterButton).Click();
+            driver.FindElement(InputEmailButton).SendKeys(userEmail);
+            driver.FindElement(InputPasswordButton).SendKeys(userPassword);
+            driver.FindElement(SubmitButton).Click();
+        }
+
+        public string GetInformationAboutUserName()
+        {
+            WaitUntilElementIsDisplayed(UserNameButton, driver);
+            string value = driver.FindElement(UserNameButton).Text;
+            return value;
+        }
+
+    }
+}
