de4de127a68d6ab20ffd996862f33c5389fbe010
NastyaZhilinskaya <nastyazhilinskaya@coherentsolutions.com>
2019-03-04 20:10:34 +0300

    Add solution with screenshot taker method.

---
+++ Task80Screenshots/ScreenshotsTask80/ScreenshotsTask80/Tests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ScreenshotsTask80.Tests
+{
+    public class BaseTest
+    {
+        public void TakeScreenshot(string directory, string name, ChromeDriver chromeDriver)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            string screenshotFileName = string.Format("{0}{1}.{2}", name, DateTime.Now.ToString("dd.MM"), ImageFormat.Jpeg.ToString().ToLowerInvariant());
+            string pathToScreenshot = Path.Combine(directory, screenshotFileName);
+            using (Image screenshot = Image.FromStream(new MemoryStream(((ITakesScreenshot)chromeDriver).GetScreenshot().AsByteArray)))
+            {
+                screenshot.Save(pathToScreenshot);
+            }
+            TestContext.AddTestAttachment(pathToScreenshot, null);
+        }
+    }
+}
