df76bff131b6f7dc99b97169a0716f5d0c1ac5e0
NastyaZhilinskaya <nastyazhilinskaya@coherentsolutions.com>
2019-03-04 15:19:05 +0300

    Add solution with Page Object pattern.

---
+++ Task70PageObject/PageObjectTask70/PageObjectTask70/Tests/TutByTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using PageObjectTask70.PageObject;
+using System.Threading;
+
+namespace PageObjectTask70.Tests
+{
+    [TestFixture]
+    class TutByTests
+    {
+        private ChromeDriver chromeDriver;
+        const string expectedUserName = "Selenium Test";
+
+        [SetUp]
+        public void SetUp()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            chromeDriver.Url = "https://www.tut.by/";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            chromeDriver.Quit();
+        }
+
+        [Test]
+        [TestCase("seleniumtests@tut.by", "123456789zxcvbn")]
+        [TestCase("seleniumtests2@tut.by", "123456789zxcvbn")]
+        public void CorrectUserLogin(string login, string password)
+        {
+            MainPage mainPage = new MainPage(chromeDriver);
+            Thread.Sleep(1500);
+
+            mainPage.LogIn(login, password);
+            Assert.AreEqual(expectedUserName, mainPage.GetInformationAboutUserName(), "User names are not equal.");
+        }
+
+        [Test]
+        [TestCase("seleniumtests@tut.by", "123456789zxcvbn")]
+        [TestCase("seleniumtests2@tut.by", "123456789zxcvbn")]
+        public void LogoutTest(string login, string password)
+        {
+            MainPage mainPage = new MainPage(chromeDriver);
+            Thread.Sleep(1500);
+
+            mainPage.LogIn(login, password);
+            Assert.AreEqual(expectedUserName, mainPage.GetInformationAboutUserName(), "User names are not equal.");
+
+            mainPage.Logout();
+            Assert.True(mainPage.CheckIfOpenMainPage(), "Main page is not open.");
+        }
+    }
+}
