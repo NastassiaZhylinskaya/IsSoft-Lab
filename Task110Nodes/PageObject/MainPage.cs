d7b37dcba2dd9f8bb43739dabf842a0742fa3015
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2019-03-12 18:24:22 +0300

    Add files via upload

---
+++ Task110Nodes/PageObject/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+
+namespace Task110Nodes.PageObject
+{
+    public class MainPage : BasePage
+    {
+        private IWebDriver driver;
+        private By EnterButton = By.LinkText("Войти");
+        private By InputEmailButton = By.Name("login");
+        private By InputPasswordButton = By.XPath("//input[@name = 'password']");
+        private By SubmitButton = By.CssSelector(".auth__enter");
+        private By UserNameButton = By.ClassName("uname");
+        private By LogoutButton = By.XPath("//*[@id='authorize']//a[contains(@href, 'logout')]");
+
+        public MainPage(IWebDriver driver)
+        {
+             this.driver = driver;
+        }
+
+        public void LogIn(string userEmail, string userPassword)
+        {
+            driver.FindElement(EnterButton).Click();
+            driver.FindElement(InputEmailButton).SendKeys(userEmail);
+            driver.FindElement(InputPasswordButton).SendKeys(userPassword);
+            driver.FindElement(SubmitButton).Click();
+        }
+
+        public string GetInformationAboutUserName()
+        {
+            WaitUntilElementIsDisplayed(UserNameButton, driver);
+            string value = driver.FindElement(UserNameButton).Text;
+            return value;
+        }
+
+        public void Logout()
+        {
+            driver.FindElement(UserNameButton).Click();
+            driver.FindElement(LogoutButton).Click();
+        }
+
+        public bool CheckIfOpenMainPage() => driver.FindElement(EnterButton).Displayed;
+    }
+}
