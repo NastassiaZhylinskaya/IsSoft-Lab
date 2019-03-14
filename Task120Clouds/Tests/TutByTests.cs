c7778b25c29df402ce35c20f3e720dc5886cbc59
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2019-03-14 14:35:03 +0300

    Add files via upload

---
+++ Task120Clouds/Tests/TutByTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using Task110Nodes.PageObject;
+
+namespace Task110Nodes.Tests
+{
+    [TestFixture("Windows 10", "MicrosoftEdge", "18")]
+    [TestFixture("Windows 8.1", "Firefox", "39.0")]
+    [TestFixture("Linux", "Chrome", "40")]
+    public class TutByTests : BaseTest
+    {
+        const string expectedUserName = "Selenium Test";
+        private string pathToScreenshots;
+
+        public TutByTests(string operatingSystem, string browserName, string version) : base(operatingSystem, browserName, version)
+        {
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            pathToScreenshots = $"{TestContext.CurrentContext.TestDirectory}\\Screenshots";
+        }
+
+        [TestCase("seleniumtests@tut.by", "123456789zxcvbn")]
+        [TestCase("seleniumtests2@tut.by", "123456789zxcvbn")]
+        public void CorrectUserLogin(string login, string password)
+        {
+            MainPage mainPage = new MainPage(driver);
+            Thread.Sleep(1500);
+
+            mainPage.LogIn(login, password);
+            Assert.AreEqual(expectedUserName, mainPage.GetInformationAboutUserName(), "User names are not equal.");
+        }
+        
+        [TestCase("seleniumtests@tut.by", "123456789zxcvbn")]
+        public void LogoutTest(string login, string password)
+        {
+            MainPage mainPage = new MainPage(driver);
+            Thread.Sleep(1500);
+
+            mainPage.LogIn(login, password);
+            Assert.AreEqual(expectedUserName, mainPage.GetInformationAboutUserName(), "User names are not equal.");
+
+            mainPage.Logout();
+            Assert.True(mainPage.CheckIfOpenMainPage(), "Main page is not open.");
+        }
+    }
+}
